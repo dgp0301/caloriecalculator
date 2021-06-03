bfa4737f4e09549b4fe886282546d90caedc409d
Dan <daniel@rpimentel.com>
2021-06-02 22:14:51 -0400

    tailwind install and basic navbar design started

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,10 +1,11 @@
 <template>
-  <nav>
-    <ul>
-      <li>Home</li>
-      <li>BMR</li>
-      <li>Macros</li>
-      <li>Info</li>
+  <nav class="container mx-auto flex flex-row justify-between ">
+    <h1 class="font-bold text-left">Calorie Counter</h1>
+    <ul class="flex flex-row justify-end">
+      <li><a class="px-4">Home</a></li>
+      <li><a class="px-4">BMR</a></li>
+      <li><a class="px-4">Macros</a></li>
+      <li><a class="px-4">Info</a></li>
     </ul>
   </nav>
 </template>
